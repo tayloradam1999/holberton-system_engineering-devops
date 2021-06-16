9338c8fd6deff8e3de9f259b77f30225ca28edc3
tayloradam1999 <tayloradam1999@gmail.com>
2021-06-16 19:31:51 +0000

    Fixing pep8 errors and added documentation

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,32 +3,32 @@
 import requests
 import sys
 
+
 def get_employee_tasks(employeeId):
+    """ Get employee to do tasks """
 
     name = ''
     task_list = []
     completed_counter = 0
 
-    userRes = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(employeeId))
-    todoRes = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'.format(employeeId))
+    userRes = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                           .format(employeeId))
+    todo = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
+                        .format(employeeId))
 
     name = userRes.json().get('name')
-    print("Name: {}".format(name))
-    
-    todoJson = todoRes.json()
+    tasks = todo.json()
 
-    for task in todoJson:
+    for task in tasks:
         if task.get('completed') is True:
             completed_counter += 1
             task_list.append(task.get('title'))
     print("task_list: {}".format(task_list))
-    print("Employee {} is done with tasks({}/{}):".format(name, completed_counter, len(todoJson)))
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, completed_counter, len(tasks)))
 
     for title in task_list:
         print("\t {}".format(title))
-
-    return 0
-
 
 if __name__ == "__main__":
     get_employee_tasks(sys.argv[1])
