73d83c5a231dedd46882aa76625738eb3e88e487
tayloradam1999 <tayloradam1999@gmail.com>
2021-06-16 19:36:17 +0000

    Pushing new var names

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,28 +12,24 @@ import sys
 def get_employee_tasks(employeeId):
     """ Get employee to do tasks """
 
-    name = ''
     task_list = []
     completed_counter = 0
 
-    userRes = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                           .format(employeeId))
+    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                        .format(argv[1]))
     todo = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
-                        .format(employeeId))
+                        .format(argv[1]))
 
-    name = userRes.json().get('name')
+    name = user.json().get('name')
     tasks = todo.json()
 
     for task in tasks:
         if task.get('completed') is True:
             completed_counter += 1
             task_list.append(task.get('title'))
-    print("task_list: {}".format(task_list))
+
     print("Employee {} is done with tasks({}/{}):"
           .format(name, completed_counter, len(tasks)))
 
-    for title in task_list:
+    for task in task_list:
         print("\t {}".format(title))
-
-if __name__ == "__main__":
-    get_employee_tasks(sys.argv[1])
